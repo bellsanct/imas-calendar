594d0c4fd190818e08844fba41cc2f1c2be25287
bellsanct <bellsanct@gmail.com>
2022-06-11 22:36:22 +0900

    障害発生につきデレのみ一時稼働停止

--- calendar.php
+++ calendar.php
@@ -18,7 +18,7 @@ $service = new Google_Service_Calendar($client);
 //calendar id
 $calendar_ids = [
   "765" => "e5misl0u751hbcjb05pmrb9grc@group.calendar.google.com",
-  "cg" => "bt23tob13vcs7b031ek6v1plos@group.calendar.google.com",
+//  "cg" => "bt23tob13vcs7b031ek6v1plos@group.calendar.google.com",
   "ml" => "0sdrrc665qu1rtql8bvr3jhfc4@group.calendar.google.com",
   "sc" => "ggplrmmqnhclf314i15fvp4m4g@group.calendar.google.com",
   "sm" => "9m2nlm3s1s1av97ol0qlffahng@group.calendar.google.com",

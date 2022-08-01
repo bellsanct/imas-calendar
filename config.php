320d0111a7771ac19d80fd54f12f9d8ec3e5a8b3
bellsanct <bellsanct@gmail.com>
2022-08-01 10:12:11 +0900

    CGの設定追加

--- config.php
+++ config.php
@@ -12,7 +12,7 @@ $prefixes = [
 //登録先のカレンダーID
 $calendar_id = [
   "765" => "e5misl0u751hbcjb05pmrb9grc@group.calendar.google.com",
-//  "cg" => "bt23tob13vcs7b031ek6v1plos@group.calendar.google.com",
+  "cg" => "bt23tob13vcs7b031ek6v1plos@group.calendar.google.com",
   "ml" => "0sdrrc665qu1rtql8bvr3jhfc4@group.calendar.google.com",
   "sc" => "ggplrmmqnhclf314i15fvp4m4g@group.calendar.google.com",
   "sm" => "9m2nlm3s1s1av97ol0qlffahng@group.calendar.google.com",
